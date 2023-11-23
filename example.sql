1fbd9b9d1bb477202c91864eb02debeffbc4ac1b
Volkan Demir <75981598+vhdemir@users.noreply.github.com>
2023-11-23 16:16:59 +0100

    Update example.sql
    create of the database, table countries and insert of initial data.

--- example.sql
+++ example.sql
@@ -1,3 +1,23 @@
-create database myDB;
+show databases;
 
-select databases;
+create database m290_LB2;
+
+use m290_LB2;
+
+CREATE TABLE IF NOT EXISTS countries (
+  countryID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  code VARCHAR(3)
+);
+
+INSERT INTO countries (countryID, name, code) VALUES
+  (0, 'Schweiz', 'CH'),
+  (0, 'Deutschland', 'DE'),
+  (0, 'Vereinigte Staaten', 'USA'),
+  (0, 'Österreich', 'AUS');
+
+select * from countries;
+
+
+
+
